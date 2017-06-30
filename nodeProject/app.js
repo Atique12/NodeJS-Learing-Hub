7c747ec087c0f4b3d624905dbdfbe9b61d13d919
Atique Ahmed <atique@drillbox.com>
2017-06-30 17:17:42 +0530

    bower Json File
    Bower Json file Used for Boot Strap

--- nodeProject/app.js
+++ nodeProject/app.js
@@ -11,6 +11,17 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 2212;
 
+// We tell Express to look into the first Public folder
+//Get to know if the static directories working fine or not go to the browser and
+// type /css/agency.css
+// If we see the content then we can say static method is working fine for us
+app.use(express.static('public'));
+
+//It will check first If any request not found in public It will go to scr/views before going app.get, app.post, app.delete
+app.use(express.static('src/views'));
+//After that type in browser /index.html or nothing by default it will go to the home Page
+
+
 app.get('/', function(req, res){
   res.send('Welcome to the Homepage'+'\n'+'This is the most cool website!!!');
 });
